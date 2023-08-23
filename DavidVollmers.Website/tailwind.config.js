bce2fad15ba7d790119a4598f50d4a6bd27548e4
dvolper <github@david.vollmers.org>
2023-08-23 08:59:37 +0200

    tailwind config

---
+++ DavidVollmers.Website/tailwind.config.js
@@ -0,0 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        '**/*.{razor,html,cshtml,js}',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Golos Text', ...defaultTheme.fontFamily.sans],
+                display: ['Ubuntu Mono', ...defaultTheme.fontFamily.sans],
+            },
+            colors: {
+                gunmetal: '#31393e',
+                'lavender-blush': '#eee5e9'
+            }
+        },
+    },
+    plugins: [require('@tailwindcss/typography')],
+}
